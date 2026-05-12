53ab7cb80fde01b0b85fabfdb424a3a7e026c9ff
Shirayuki39 <lorddemecrius83@proton.me>
2026-05-12 12:07:46 +0800

    X6882: Update usb soong config to bool type
    * Extension to 6b7e037
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: jvaswb <bpsixta@gmail.com>
    Signed-off-by: scaledzdn. <72336864+zaidannn7@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -453,7 +453,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # USB
-$(call soong_config_set,android_hardware_mediatek_usb,audio_accessory_supported,true)
+$(call soong_config_set_bool,android_hardware_mediatek_usb,audio_accessory_supported,true)
 
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
