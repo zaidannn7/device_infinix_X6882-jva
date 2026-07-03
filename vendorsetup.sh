bd869dfe45399e2808f63953801077caecdfb0d4
scaledzdn. <72336864+zaidannn7@users.noreply.github.com>
2026-07-03 22:45:17 +0000

    X6882: init vendor setup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+rm -rf device/infinix/X6882-kernel
 
+git clone --depth=1 https://github.com/javas-prjkt/device_infinix_X6882-kernel device/infinix/X6882-kernel
+
+rm -rf hardware/mediatek device/mediatek/sepolicy_vndr
+git clone --depth=1 -b sixteen-oem https://github.com/MillenniumOSS/android_vendor_mediatek_ims vendor/mediatek/ims
+git clone --depth=1 https://github.com/zaidannn7/hardware_transsion hardware/transsion
+git clone --depth=1 https://github.com/MillenniumOSS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+git clone --depth=1 https://github.com/MillenniumOSS/android_hardware_mediatek hardware/mediatek
+#git clone https://github.com/MillenniumOSS/android_device_millennium_common-kernel device/millennium/common-kernel
+rm -rf packages/apps/ViPER4AndroidFX
+rm -rf packages/Apps/ViPER4AndroidFX
+git clone https://github.com/hound-lab/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 RET=0
 echo "- Applying Aperture Mediatek HFPS Mode"
 cd packages/apps/Aperture
@@ -21,3 +33,6 @@ if [ $RET -ne 0 ]; then
 else
   echo "OK: All patched"
 fi
+
+export BUILD_USERNAME=zaidanprjkt
+export BUILD_HOSTNAME=android
